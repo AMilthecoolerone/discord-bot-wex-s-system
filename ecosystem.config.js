adcb5b17cf46a0a0a2c896c12624b3cf77d5d193
amil dev <155320813+AMilthecoolerone@users.noreply.github.com>
2026-01-06 16:38:33 +0100

    Ensuring PM2 Runs from thhe project root

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = {
   apps: [
     {
       name: 'katapump-mod-bot',
       script: 'src/index.js',
+      cwd: path.resolve(__dirname), // Ensure PM2 runs from project root
       env: {
         NODE_ENV: 'production'
       }
